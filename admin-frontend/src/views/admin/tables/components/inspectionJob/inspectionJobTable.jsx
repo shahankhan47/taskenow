510bdb5a8e147cf829cdbac8bbf77ea672ea53bd
Shahan Khan <prince47khan@gmail.com>
2023-10-21 17:58:15 +0530

    updated table

--- admin-frontend/src/views/admin/tables/components/inspectionJob/inspectionJobTable.jsx
+++ admin-frontend/src/views/admin/tables/components/inspectionJob/inspectionJobTable.jsx
@@ -1,20 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  useGlobalFilter,
-  usePagination,
-  useSortBy,
-  useTable,
-} from "react-table";
+
 import Card from "components/card";
-import {
-  MdCheckCircle,
-  MdPending,
-  MdCancel,
-  MdAssignmentLate
-} from "react-icons/md";
-import Progress from "components/progress";
+
 import JobDetailsModal from "./jobDetailsModel";
 import { getJobsofType, deleteJob } from "data/api";
+import MUIDataTable from "mui-datatables";
 
 let tableData = [
   {
@@ -39,28 +29,7 @@ getJobsofType({type: "Inspection"}).then((jobs) => {
   tableData = tableData.concat(jobs.data)
 })
 
-const columnsData = [
-  {
-    Header: "JOB ID",
-    accessor: "jobId",
-  },
-  {
-    Header: "SERVICE",
-    accessor: "service",
-  },
-  {
-    Header: "DATE",
-    accessor: "date",
-  },
-  {
-    Header: "STATUS",
-    accessor: "status",
-  },
-  {
-    Header: "SEE DETAILS",
-    accessor: "details",
-  },
-];
+
 
 const DevelopmentTable = () => {
   const [showModal, setShowModal] = useState(false);
@@ -72,25 +41,61 @@ const DevelopmentTable = () => {
       setJobData(jobs.data)
     })
   }, [])
-
-  const tableInstance = useTable(
+  
+  const columnsData = [
     {
-      columns: columnsData,
-      data: jobData,
-      initialState: { pageSize: 5 }, // Set the initial page size
+      name: "jobId",
+      label: "JOB ID",
+      options: {
+        filter: true,
+        sort: true,
+      }
     },
-    useGlobalFilter,
-    useSortBy,
-    usePagination
-  );
+    {
+      name: "service",
+      label: "SERVICE",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "date",
+      label: "DATE",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "status",
+      label: "STATUS",
+      options: {
+        filter: true,
+        sort: true,
+      }
+    },
+    {
+      name: "details",
+      label: "SEE DETAILS",
+      options: {
+        filter: true,
+        sort: true,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <button
+              className="px-2 py-2 text-blue-800 bg-gradient-to-r from-sky-500 to-blue-500 hover:from-sky-600 hover:to-blue-600 border border-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+              onClick={() => openModal(tableData[tableMeta.rowIndex])}
+            >
+              Details
+            </button>
+          );
+        },
+      },
+    },
+  ];
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    page,
-    prepareRow,
-  } = tableInstance;
+
 
   const deleteSelectedJob = async () => {
     await deleteJob(selectedJob);
@@ -112,127 +117,27 @@ const DevelopmentTable = () => {
   };
 
   return (
-    <Card extra={"w-full h-full p-4 sm:overflow-x-auto"}>
-      <div className="relative flex items-center justify-between">
-        <div className="text-xl font-bold text-navy-700 dark:text-white">
-          Job ( Inspection )
-        </div>
-      </div>
-
-      <div className="mt-8 h-full overflow-x-scroll xl:overflow-hidden">
-        <table {...getTableProps()} className="w-full">
-          <thead>
-            {headerGroups.map((headerGroup, index) => (
-              <tr {...headerGroup.getHeaderGroupProps()} key={index}>
-                {headerGroup.headers.map((column, index) => (
-                  <th
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                    key={index}
-                    className="border-b border-gray-200 pr-24 pb-[10px] text-start dark:!border-navy-700"
-                  >
-                    <p className="text-xs tracking-wide text-gray-600">
-                      {column.render("Header")}
-                    </p>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {page.map((row, index) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()} key={index}>
-                  {row.cells.map((cell, index) => {
-                    let data = "";
-                    if (cell.column.Header === "NAME") {
-                      data = (
-                        <p className="text-sm font-bold text-navy-700 dark:text-white">
-                          {cell.value}
-                        </p>
-                      );
-                    } else if (cell.column.Header === "SERVICE") {
-                      data = (
-                        <p className="text-sm font-bold text-navy-700 dark:text-white">
-                          {cell.value}
-                        </p>
-                      );
-                    } else if (cell.column.Header === "DATE") {
-                      data = (
-                        <p className="text-sm font-bold text-navy-700 dark:text-white">
-                          {cell.value}
-                        </p>
-                      );
-                    } else if (cell.column.Header === "PROGRESS") {
-                      data = (
-                        <div className="flex items-center gap-3">
-                          <p className="text-sm font-bold text-navy-700 dark:text-white">
-                            {cell.value}%
-                          </p>
-                          <Progress width="w-[68px]" value={cell.value} />
-                        </div>
-                      );
-                    } else if (cell.column.Header === "JOB ID") {
-                      data = (
-                        <p className="text-sm font-bold text-navy-700 dark:text-white">
-                          {cell.value}
-                        </p>
-                      );
-                    } else if (cell.column.Header === "STATUS") {
-                      data = (
-                        <div className="flex items-center gap-2">
-                          <div className={`rounded-full text-xl`}>
-                            {cell.value === "Accepted" ? (
-                              <MdCheckCircle className="text-green-500" />
-                            ) : cell.value === "Pending" ? (
-                              <MdPending className="text-yellow-500" />
-                            ) : cell.value === "Declined" ? (
-                              <MdCancel className="text-red-500" />
-                            ) : cell.value === "Unassigned" ? (
-                              <MdAssignmentLate className="text-gray-500" />
-                            ) : null}
-                          </div>
-                          <p className="text-sm font-bold text-navy-700 dark:text-white">
-                            {cell.value}
-                          </p>
-                        </div>
-                      );
-                    } else if (cell.column.Header === "SEE DETAILS") {
-                      data = (
-                        <button
-                          className="px-2 py-2 text-blue-800 bg-gradient-to-r from-sky-500 to-blue-500 hover:from-sky-600 hover:to-blue-600 border border-transparent rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                          onClick={() => openModal(row.original)}
-                        >
-                          Details
-                        </button>
-                      );
-                    }
-                    return (
-                      <td
-                        className="pt-[14px] pb-[18px] sm:text-[14px]"
-                        {...cell.getCellProps()}
-                        key={index}
-                      >
-                        {data}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      {/* Display the modal when showModal is true */}
-      <JobDetailsModal 
-      isOpen={showModal} 
-      onClose={closeModal} 
-      job={selectedJob} 
-      deleteJob={deleteSelectedJob}
-      setJob={setSelectedJob}
+    <Card extra={"w-full h-full p-4 sm:overflow-x-auto "}>
+    <div className="mt-8 h-full overflow-x-scroll xl:overflow-hidden">
+      <MUIDataTable
+        title={"Inspection Jobs"}
+        data={tableData}
+        columns={columnsData}
+        options={{
+          selectableRows: "none", // Disable row selection
+        }}
       />
-    </Card>
+    </div>
 
+    {/* Display the modal when showModal is true */}
+    <JobDetailsModal 
+    isOpen={showModal} 
+    onClose={closeModal} 
+    job={selectedJob}
+    deleteJob={deleteSelectedJob}
+    setJob={setSelectedJob}
+    />
+  </Card>
   );
 };
 
